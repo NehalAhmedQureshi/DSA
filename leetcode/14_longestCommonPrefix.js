924a3b65b14e9f35a2b34061a3dce805ce161719
Nehal <m.nehalq125@gmail.com>
2024-08-29 19:43:15 +0500

    update readme for 14_problem

--- leetcode/14_longestCommonPrefix.js
+++ leetcode/14_longestCommonPrefix.js
@@ -6,14 +6,12 @@ function longestCommonPrefix(strs) {
     for (let i = 1; i < strs.length; i++) {
         console.log('prefix =>',prefix)
         // Keep trimming the prefix until it's a prefix of strs[i]
-        console.log(strs[i].indexOf(prefix))
+
         while (strs[i].indexOf(prefix) !== 0) {
-            console.log("second",prefix.length)
             prefix = prefix.slice(0, prefix.length - 1); // Shorten the prefix by one character
             if (prefix === "") return ""; // If no common prefix exists
         }
     }
-    console.log(prefix)
     return prefix;
 }
 longestCommonPrefix(["gap", "gasp"]); // Output: "fl"
--- leetcode/readme.md
+++ leetcode/readme.md
@@ -2,3 +2,4 @@
 
 
 - [13_romanToInteger](https://leetcode.com/problems/roman-to-integer/submissions/1372077633)
+- [14_longestCommonPrefix](https://leetcode.com/problems/longest-common-prefix/submissions/1372286752)
