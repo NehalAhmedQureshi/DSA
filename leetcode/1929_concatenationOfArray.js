b0317b8d149af414f0478579f1d9fe2dfac4c419
Nehal <m.nehalq125@gmail.com>
2024-09-02 21:42:34 +0500

    complete problem 1929

--- leetcode/1929_concatenationOfArray.js
+++ leetcode/1929_concatenationOfArray.js
@@ -1,9 +1,10 @@
 
 var getConcatenation = function(nums) {
-      nums = [1,2,1]
-      let result = nums.concat(nums)
+      // nums = [1,2,1]
+      let ans = nums.concat(nums)
       //   return result
-      console.log(result);
+      console.log(ans);
+      return(ans)
   };
 
-console.log(getConcatenation());
+console.log(getConcatenation([1,2,3,4,4]));
--- leetcode/readme.md
+++ leetcode/readme.md
@@ -3,3 +3,4 @@
 
 - [13_romanToInteger](https://leetcode.com/problems/roman-to-integer/submissions/1372077633)
 - [14_longestCommonPrefix](https://leetcode.com/problems/longest-common-prefix/submissions/1372286752)
+- [1929_concentrationOfArray](https://leetcode.com/problems/concatenation-of-array/submissions/1376717769)
